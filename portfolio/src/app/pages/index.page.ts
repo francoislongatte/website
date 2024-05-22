2b41f6a8426bf3a1b8e9f913fdd19ebd8dec60b5
francois longatte <francois.longatte@gmail.com>
2024-05-23 00:28:23 +0200

    reset

--- portfolio/src/app/pages/index.page.ts
+++ portfolio/src/app/pages/index.page.ts
@@ -9,7 +9,7 @@ import { NewsletterComponent } from '../component/newsletter/newsletter.componen
   standalone: true,
   imports: [NameComponent, BentoComponent, NewsletterComponent,  FooterComponent],
   template: `
-    <div class="bg-orange-600 w-full">
+    <div class="w-full">
       <name />
       <bento />
       <newsletter />
